bb6a475ee3eaf27f04fe8ca99b9582e9bb0d82a3
davidkneely <davidics613@gmail.com>
2018-01-25 18:33:35 -1000

    Initial Commit

---
+++ pomodoro002/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  pomodoro002
+//
+//  Created by David on 1/25/18.
+//  Copyright © 2018 Vision Runner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ pomodoro002/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  pomodoro002
+//
+//  Created by David on 1/25/18.
+//  Copyright © 2018 Vision Runner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
